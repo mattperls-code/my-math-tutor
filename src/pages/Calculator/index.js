e78650e61dd38be914a31a31156472c8fc499720
mattperls-code <mattperls.code@gmail.com>
2021-04-26 18:17:40 -0400

    v1.3.2 ~ Rewrote Calculator ~ TODO: Remove Unused Libs And Packages ~ Also Add Translations For Calculator Output

--- src/pages/Calculator/index.js
+++ src/pages/Calculator/index.js
@@ -19,6 +19,19 @@ const Calculator = () => {
 
     let [solutionElement, setSolutionElement] = useState(null)
 
+    const solve = () => {
+        if(document.getElementById("question").value.trim() == ""){
+            setSolutionElement(null)
+            return
+        }
+        let solution = calculateOutput(document.getElementById("question").value)
+        if(solution === false){
+            setSolutionElement(null)
+        } else {
+            setSolutionElement(solution)
+        }
+    }
+
     return (
         <Page smallText={
             {
@@ -58,115 +71,13 @@ const Calculator = () => {
                                 French: "Entrez un Problème Mathématique",
                                 German: "Geben Sie ein Mathematisches Problem ein"
                             }[localStorage.getItem("language")]
-                        }/>
-                        <select className="solutionTypeSelector" id="solutionType">
-                            <option>
-                                {
-                                    {
-                                        English: "Please Select A Solution Type",
-                                        Spanish: "Seleccione un Tipo de Solución",
-                                        French: "Veuillez Sélectionner un Type de Solution",
-                                        German: "Bitte Wählen Sie einen Lösungstyp"
-                                    }[localStorage.getItem("language")]
-                                }
-                            </option>
-                            <option>
-                                {
-                                    {
-                                        English: "Evaluate Expression",
-                                        Spanish: "Evaluar Expresión",
-                                        French: "Évaluer l'Expression",
-                                        German: "Ausdruck Auswerten"
-                                    }[localStorage.getItem("language")]
-                                }
-                            </option>
-                            <option>
-                                {
-                                    {
-                                        English: "Rationalize",
-                                        Spanish: "Racionalizar",
-                                        French: "Rationaliser",
-                                        German: "Rationalisieren"
-                                    }[localStorage.getItem("language")]
-                                }
-                            </option>
-                            <option>
-                                {
-                                    {
-                                        English: "Solve For Variable",
-                                        Spanish: "Resolver para Variable",
-                                        French: "Résoudre pour la Variable",
-                                        German: "Nach Variablen lösen"
-                                    }[localStorage.getItem("language")]
-                                }
-                            </option>
-                            <option>
-                                {
-                                    {
-                                        English: "Find Derivative",
-                                        Spanish: "Encontrar derivada",
-                                        French: "Rechercher un Dérivé",
-                                        German: "Derivat Finden"
-                                    }[localStorage.getItem("language")]
-                                }
-                            </option>
-                        </select>
-                        <div className="solveButton" onClick={() => {
-                            if(document.getElementById("solutionType").value == 
-                                {
-                                    English: "Please Select A Solution Type",
-                                    Spanish: "Seleccione un Tipo de Solución",
-                                    French: "Veuillez Sélectionner un Type de Solution",
-                                    German: "Bitte Wählen Sie einen Lösungstyp"
-                                }[localStorage.getItem("language")]
-                            ){
-                                alert("Please Select A Solution Type")
-                                return
+                        } onKeyUp={(e) => {
+                            if(e.key == "Enter"){
+                                solve()
                             }
-                            let solution = calculateOutput()
-                            if(solution.successful){
-                                solution.value = solution.value.replaceAll("*", "•").replaceAll(",", ", ")
-                                if(solution.value == "1" && document.getElementById("solutionType").value == 
-                                    {
-                                        English: "Evaluate Expression",
-                                        Spanish: "Evaluar Expresión",
-                                        French: "Évaluer l'Expression",
-                                        German: "Ausdruck Auswerten"
-                                    }[localStorage.getItem("language")]
-                                ){
-                                    setSolutionElement(
-                                        <React.Fragment>
-                                            <div className="text">Solution:<br/><strong>{solution.value}</strong></div>
-                                            <div className="warning">We Recommend You Check To Make Sure There Are Not Infinite Solutions</div>
-                                        </React.Fragment>
-                                    )
-                                } else if(solution.value == "No Solution"){
-                                    setSolutionElement(
-                                        <React.Fragment>
-                                            <div className="text">Solution: <strong>{solution.value}</strong></div>
-                                            <div className="warning">We Recommend That You Check This Answer, It May Be Wrong</div>
-                                        </React.Fragment>
-                                    )
-                                } else if(solution.value == ""){
-                                    setSolutionElement(
-                                        <div className="text">Solution: <strong>No Real Solutions</strong></div>
-                                    )
-                                } else {
-                                    setSolutionElement(
-                                        <div className="text">Solution: <strong>{solution.value}</strong></div>
-                                    )
-                                }
-                            } else {
-                                setSolutionElement(
-                                    <div className="error">
-                                        {
-                                            solution.error
-                                        }
-                                    </div>
-                                )
-                            }
-                        }}>
-                            <span>
+                        }}/>
+                        <div className="solveButton" onClick={solve}>
+                            <div className="goText">
                                 {
                                     {
                                         English: "Go",
@@ -175,13 +86,21 @@ const Calculator = () => {
                                         German: "Gehen"
                                     }[localStorage.getItem("language")]
                                 }
-                            </span>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className="solution">
                     {
-                        solutionElement
+                        (() => {
+                            if(solutionElement == null){
+                                return
+                            } else if(solutionElement.successful){
+                                return <div className="text">{ solutionElement.output.replaceAll(",", ",\n") }</div>
+                            } else {
+                                return <div className="error">{ solutionElement.output }</div>
+                            }
+                        })()
                     }
                 </div>
             </main>
--- src/scripts/calculatorOutput.js
+++ src/scripts/calculatorOutput.js
@@ -6,262 +6,80 @@
     •--------------------------------•
 */
 
-import { simplify, rationalize, derivative } from "mathjs"
 import { parse } from "algebra.js"
 
-const calculateOutput = () => {
-
-    // ! FIX THIS WHOLE FUNCTION AND DOCUMENT IT
-
-    // ! Remove "Specify Solution Type" and default to evaluate
-    // ! Add intuitive solution types
-    // ! Remove rationalization ( Its effectively the same as evaluating )
-    // ! Potentially add in a gui cmoponent for declaring and specifying variables in calculator
-
-    const solutionType = document.getElementById("solutionType").value
-
-    let questionInput = document.getElementById("question").value.trim()
-    if(questionInput[questionInput.length-1] == ";"){
-        questionInput = questionInput.slice(0, questionInput.length-1)
-    }
-
-    let [problem, scope, target] = ["", {}, ""]
-
-    if(questionInput.split(";").length == 1){
-        problem = ""+questionInput
-    } else if(questionInput.split(";").length == 2){
-        problem = questionInput.split(";")[0]
-        if(questionInput.split(";")[1].includes("=")){
-            questionInput.split(";")[1].split(",").forEach(variable => {
-                if(variable.split("=").length != 2){
-                    return {
-                        successful: false,
-                        error: "Syntax Error: Variables Should Be Assigned Like \"x = 2\" And Should Be Seperated By A Comma"
-                    }
-                }
-                const varName = variable.split("=")[0].trim()
-                const varValue = variable.split("=")[1].trim()
-                scope[varName] = Number(varValue)
-            })
-        } else {
-            target = questionInput.split(";")[1].trim()
-        }
-    } else if(questionInput.split(";").length == 3){
-        problem = questionInput.split(";")[0]
-        if(questionInput.split(";")[1].includes("=")){
-            questionInput.split(";")[1].split(",").forEach(variable => {
-                if(variable.split("=").length != 2){
-                    return {
-                        successful: false,
-                        error: "Syntax Error: Variables Should Be Assigned Like \"x = 2\" And Should Be Seperated By A Comma"
-                    }
-                }
-                const varName = variable.split("=")[0].trim()
-                const varValue = variable.split("=")[1].trim()
-                scope[varName] = Number(varValue)
-            })
-            target = questionInput.split(";")[2].trim()
-        } else if(questionInput.split(";")[2].includes("=")) {
-            questionInput.split(";")[2].split(",").forEach(variable => {
-                if(variable.split("=").length != 2){
-                    return {
-                        successful: false,
-                        error: "Syntax Error: Variables Should Be Assigned Like \"x = 2\" And Should Be Seperated By A Comma"
-                    }
-                }
-                const varName = variable.split("=")[0].trim()
-                const varValue = variable.split("=")[1].trim()
-                scope[varName] = Number(varValue)
-            })
-            target = questionInput.split(";")[1].trim()
-        } else {
-            return {
-                successful: false,
-                error: "Syntax Error: Expected A Variable To Be Assigned, But None Were"
-            }
-        }
-    } else {
-        return {
-            successful: false,
-            error: "Syntax Error: An Invalid Number Of Parameters Were Given"
-        }
-    }
-
-    const hasScope = Object.keys(scope).length > 0
-    if(solutionType == 
-        {
-            English: "Evaluate Expression",
-            Spanish: "Evaluar Expresión",
-            French: "Évaluer l'Expression",
-            German: "Ausdruck Auswerten"
-        }[localStorage.getItem("language")]
-    ){
-        if(problem.includes("=")){
-            return {
-                successful: false,
-                error: "Error In Problem: The Problem You Entered Was An Equation Not An Expression"
-            }
-        }
-        if(hasScope){
-            return {
-                successful: true,
-                value: simplify(problem, scope).toString()
-            }
-        } else {
-            return {
-                successful: true,
-                value: simplify(problem).toString()
-            }
-        }
-    } else if(solutionType == 
-        {
-            English: "Rationalize",
-            Spanish: "Racionalizar",
-            French: "Rationaliser",
-            German: "Rationalisieren"
-        }[localStorage.getItem("language")]
-    ){
-        if(problem.includes("=")){
-            return {
-                successful: false,
-                error: "Error In Problem: The Problem You Entered Was An Equation Not An Expression"
-            }
-        }
-        if(hasScope){
-            return {
-                successful: true,
-                value: rationalize(problem, scope).toString()
-            }
-        } else {
-            return {
-                successful: true,
-                value: rationalize(problem).toString()
-            }
-        }
-    } else if(solutionType == 
-        {
-            English: "Solve For Variable",
-            Spanish: "Resolver para Variable",
-            French: "Résoudre pour la Variable",
-            German: "Nach Variablen lösen"
-        }[localStorage.getItem("language")]
-    ){
-        if(!problem.includes("=")){
-            return {
-                successful: false,
-                error: "Error In Problem: The Problem You Entered Was An Expression Not An Equation"
-            }
-        }
-        if(target.length != 1){
-            return {
-                successful: false,
-                error: "Error In Problem: You Did Not Specify A Valid Variable To Solve For. Specify A Variable By Adding \"; variable\" At The End Of Your Problem"
-            }
-        }
-        // Algebra.js allows For Implicit Multiplication And Variable Scope
-        if(hasScope){
-            const side1 = simplify(problem.split("=")[0], scope).toString()
-            const side2 = simplify(problem.split("=")[1], scope).toString()
-            if(!problem.split("=")[0].includes(target) && !problem.split("=")[1].includes(target)){
+const calculateOutput = (problem) => {
+    try {
+        for(let i = 0;i<problem.length;i++){
+            // Make sure all input is valid
+            if(!"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+-*/=^.()[] ".includes(problem[i])){
                 return {
                     successful: false,
-                    error: "Error In Problem: Variable Not Present In Problem"
+                    output: `Problem Contains Illegal Character "${problem[i]}"`
                 }
             }
-            if(side1 == side2){
-                return {
-                    successful: true,
-                    value: "Infinite Solutions"
+        }
+        if(problem.includes("=")){
+            let activeVariables = []
+            for(let i = 0;i<problem.length;i++){
+                if("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".includes(problem[i]) && activeVariables.indexOf(problem[i]) == -1){
+                    activeVariables.push(problem[i])
                 }
             }
-            try {
-                const targetValue = parse(side1 + "=" + side2).solveFor(target)
-                if(targetValue == undefined){
-                    return {
-                        successful: false,
-                        error: "Internal Error: Unable To Solve This Equation, Sorry"
-                    }
-                } else {
-                    return {
-                        successful: true,
-                        value: targetValue.toString()
-                    }
-                }
-            } catch {
-                return {
-                    successful: true,
-                    value: "No Solution"
+            let previousPrompt = false
+            let targetVar = activeVariables.length == 1 ? activeVariables[0] : false
+            // As long as it is not defined, keep asking
+            while(targetVar === false){
+                let variable = prompt(previousPrompt ? "The Variable You Entered Was Not Valid. Please Try Again" : "Please Enter The Variable You Want To Solve For")
+                previousPrompt = true
+                if(activeVariables.includes(variable)){
+                    targetVar = variable
+                } else if(variable == ""){
+                    // Makes loop user breakable
+                    return false;
                 }
             }
-        } else {
-            const side1 = simplify(problem.split("=")[0]).toString()
-            const side2 = simplify(problem.split("=")[1]).toString()
-            if(!problem.split("=")[0].includes(target) && !problem.split("=")[1].includes(target)){
+            const solution = parse(problem)
+            if(solution == undefined){
                 return {
                     successful: false,
-                    error: "Error In Problem: Variable Not Present In Problem"
+                    output: "Unable To Find Solution. This May Mean The Problem Has No Real Solutions Or That There Is No Definite Answer"
                 }
-            }
-            if(side1 == side2){
-                return {
-                    successful: true,
-                    value: "Infinite Solutions"
-                }
-            }
-            try {
-                const targetValue = parse(side1 + "=" + side2).solveFor(target)
-                if(targetValue == undefined){
+            } else {
+                const variableValue = solution.solveFor(targetVar)
+                if(variableValue == undefined){
                     return {
                         successful: false,
-                        error: "Internal Error: Unable To Solve This Equation, Sorry"
-                    }
-                } else {
-                    return {
-                        successful: true,
-                        value: targetValue.toString()
+                        output: "Unable To Find Solution. This May Mean The Problem Has No Real Solutions Or That There Is No Definite Answer"
                     }
                 }
-            } catch {
                 return {
                     successful: true,
-                    value: "No Solution"
+                    output: variableValue.toString()
+                }
+            }
+            
+        } else if(problem.includes("<") || problem.includes(">") || problem.includes("≤") || problem.includes("≥")){
+            alert("Sorry, We Don't Currently Support Inequalities. Hopefully We Will In The Future.")
+            return false
+        } else {
+            const solution = parse(problem)
+            if(solution == undefined){
+                return {
+                    successful: false,
+                    output: "Unable To Find Solution. This May Mean The Problem Has No Real Solutions Or That There Is No Definite Answer"
+                }
+            } else {
+                return {
+                    successful: true,
+                    output: solution.toString()
                 }
             }
         }
-    } else if(solutionType == 
-        {
-            English: "Find Derivative",
-            Spanish: "Encontrar derivada",
-            French: "Rechercher un Dérivé",
-            German: "Derivat Finden"
-        }[localStorage.getItem("language")]
-    ){
-        if(problem.includes("=")){
-            return {
-                successful: false,
-                error: "Error In Problem: The Problem You Entered Was An Equation Not An Expression"
-            }
-        }
-        if(target.length == 1){
-            return {
-                successful: true,
-                value: derivative(problem, target, { simplify: true }).toString()
-            }
-        } else if(problem.includes("x")){
-            return {
-                successful: true,
-                value: derivative(problem, "x", { simplify: true }).toString()
-            }
-        } else {
-            return {
-                successful: false,
-                error: "Error In Problem: Please Specify The Variable To Calculate The Derivative With Respect To. Specify A Variable By Adding \"; variable\" At The End Of Your Problem"
-            }
-        }
-    } else {
+    } catch(error) {
         return {
             successful: false,
-            error: "An Internal Error Occcurred, Try Refreshing The Page"
+            output: "Unable To Solve. This May Be An Internal Error"
         }
     }
 }
